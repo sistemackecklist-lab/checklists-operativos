a00972f2c3710cbfc495688065baba19f34a16a3
sistemackecklist-lab <sistemackecklist@gmail.com>
2026-07-13 17:41:50 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,4 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBS9fY8txtO_TFhlhhTj4KfImCUu65cLJo",
   authDomain: "checklists-operativos.firebaseapp.com",
@@ -16,8 +8,3 @@ const firebaseConfig = {
   appId: "1:795436931731:web:16eadab3691af027eacb66",
   measurementId: "G-H9YQ9B0ZXC"
 };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
