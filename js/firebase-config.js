5976354261a2578c7d88d35c72f19540f9363a9f
sistemackecklist-lab <sistemackecklist@gmail.com>
2026-07-13 17:41:02 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -7,15 +7,15 @@
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBS9fY8txtO_TFhlhhTj4KfImCUu65cLJo",
-    authDomain: "checklists-operativos.firebaseapp.com",
-    projectId: "checklists-operativos",
-    storageBucket: "checklists-operativos.firebasestorage.app",
-    messagingSenderId: "795436931731",
-    appId: "1:795436931731:web:16eadab3691af027eacb66",
-    measurementId: "G-H9YQ9B0ZXC"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBS9fY8txtO_TFhlhhTj4KfImCUu65cLJo",
+  authDomain: "checklists-operativos.firebaseapp.com",
+  projectId: "checklists-operativos",
+  storageBucket: "checklists-operativos.firebasestorage.app",
+  messagingSenderId: "795436931731",
+  appId: "1:795436931731:web:16eadab3691af027eacb66",
+  measurementId: "G-H9YQ9B0ZXC"
+};
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
