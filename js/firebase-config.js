01a48e3ba9960768f4eb2fe0b75502690c77c94e
sistemackecklist-lab <sistemackecklist@gmail.com>
2026-07-13 21:38:43 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,17 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBS9fY8txtO_TFhlhhTj4KfImCUu65cLJo",
   authDomain: "checklists-operativos.firebaseapp.com",
   projectId: "checklists-operativos",
   storageBucket: "checklists-operativos.firebasestorage.app",
   messagingSenderId: "795436931731",
-  appId: "1:795436931731:web:16eadab3691af027eacb66",
-  measurementId: "G-H9YQ9B0ZXC"
+  appId: "1:795436931731:web:16eadab3691af027eacb66"
 };
+
+firebase.initializeApp(firebaseConfig);
+
+// Servicios que usaremos en toda la app
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+// Sesión persistente en el dispositivo ("recordar sesión")
+auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL);
