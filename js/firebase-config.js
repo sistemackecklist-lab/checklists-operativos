9d8f20eb41e95159a53ca41827d16dc328ce57be
sistemackecklist-lab <sistemackecklist@gmail.com>
2026-07-13 23:02:46 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -9,13 +9,15 @@
       reemplazá los valores de abajo por los tuyos.
    ============================================================ */
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBS9fY8txtO_TFhlhhTj4KfImCUu65cLJo",
+  authDomain: "checklists-operativos.firebaseapp.com",
+  projectId: "checklists-operativos",
+  storageBucket: "checklists-operativos.firebasestorage.app",
+  messagingSenderId: "795436931731",
+  appId: "1:795436931731:web:16eadab3691af027eacb66",
+  measurementId: "G-H9YQ9B0ZXC"
 };
 
 firebase.initializeApp(firebaseConfig);
